3cfb353515f31f9cf75bd5e6208964499f2bb74f
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-06-20 16:12:56 -0400

    add opt param to cli.ts functions

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -22,15 +22,10 @@ interface Options {
   showPlayerOrder: boolean;
 }
 
-const OPTIONS: Options = {
-  abbreviate: false,
-  showEmpty: false,
-  showPlayerOrder: true,
-};
-
-const separator = OPTIONS.abbreviate ? "," : ", ";
-
-function getChamberChunks(chamber: PlayerState["chamber"]): slChunk[] {
+function getChamberChunks(
+  chamber: PlayerState["chamber"],
+  opt: Options,
+): slChunk[] {
   // Count the quarks of each flavor
   let quarkCounts: { flavor: Flavor; count: number }[] = FLAVORS.map(
     (flavor) => {
@@ -44,16 +39,14 @@ function getChamberChunks(chamber: PlayerState["chamber"]): slChunk[] {
   // Construct the chunks
   const chunks: slChunk[] = [];
   quarkCounts.forEach(({ count, flavor }) => {
-    if (count > 0 || OPTIONS.showEmpty) {
-      const flavorString = OPTIONS.abbreviate
-        ? flavor.slice(0, 1)
-        : ` ${flavor}`;
+    if (count > 0 || opt.showEmpty) {
+      const flavorString = opt.abbreviate ? flavor.slice(0, 1) : ` ${flavor}`;
       chunks.push({
         text: `${count}${flavorString}`,
         style: QUARK_MAPPING[flavor],
       });
 
-      chunks.push({ text: separator, style: "gray" });
+      chunks.push({ text: opt.abbreviate ? "," : ", ", style: "gray" });
     }
   });
 
@@ -63,14 +56,18 @@ function getChamberChunks(chamber: PlayerState["chamber"]): slChunk[] {
   return chunks;
 }
 
-function getPlayerChunks(player: PlayerState, highlight?: boolean): slChunk[] {
+function getPlayerChunks(
+  player: PlayerState,
+  opt: Options,
+  highlight?: boolean,
+): slChunk[] {
   const chunks: slChunk[] = [];
 
   // Name
   chunks.push({ text: player.name, style: highlight ? "bold" : "white" });
 
   // Order
-  if (OPTIONS.showPlayerOrder) {
+  if (opt.showPlayerOrder) {
     if (highlight) {
       // Use square brackets instead of parenthesis so that the active player is
       // identifiable even if the colors aren't displaying.
@@ -86,22 +83,22 @@ function getPlayerChunks(player: PlayerState, highlight?: boolean): slChunk[] {
   chunks.push({ text: ": ", style: "gray" });
 
   // Chamber
-  const chamberChunks = getChamberChunks(player.chamber);
+  const chamberChunks = getChamberChunks(player.chamber, opt);
   chunks.push(...chamberChunks);
 
   // Hadrons
-  if (player.score > 0 || OPTIONS.showEmpty) {
+  if (player.score > 0 || opt.showEmpty) {
     // Only add a separator if the chamber chunks actually existed.
     if (chamberChunks.length > 0) {
-      chunks.push({ text: separator, style: "gray" });
+      chunks.push({ text: opt.abbreviate ? "," : ", ", style: "gray" });
     }
 
-    if (!OPTIONS.abbreviate) {
+    if (!opt.abbreviate) {
       chunks.push({ text: "and ", style: "gray" });
     }
 
     chunks.push({
-      text: `${player.score}${OPTIONS.abbreviate ? "h" : " hadrons"}`,
+      text: `${player.score}${opt.abbreviate ? "h" : " hadrons"}`,
       style: "bold",
     });
   }
@@ -109,7 +106,10 @@ function getPlayerChunks(player: PlayerState, highlight?: boolean): slChunk[] {
   return chunks;
 }
 
-function getStateChunks(state: CurrentGameState | PastGameState): slChunk[] {
+function getStateChunks(
+  state: CurrentGameState | PastGameState,
+  opt: Options,
+): slChunk[] {
   const chunks: slChunk[] = [];
 
   // Log the current turn
@@ -119,7 +119,7 @@ function getStateChunks(state: CurrentGameState | PastGameState): slChunk[] {
   // Log the current superposition
   chunks.push({ text: ": ", style: "gray" });
   state.activeQuark.forEach((flavor, index, superposition) => {
-    const flavorString = OPTIONS.abbreviate ? flavor.slice(0, 1) : flavor;
+    const flavorString = opt.abbreviate ? flavor.slice(0, 1) : flavor;
     chunks.push({
       text: flavorString,
       style: QUARK_MAPPING[flavor],
@@ -131,26 +131,26 @@ function getStateChunks(state: CurrentGameState | PastGameState): slChunk[] {
     // We only add separators if there's more than one element and if this
     // isn't the last element in the list.
     if (superposition.length > 1 && isNotLastElement) {
-      if (OPTIONS.abbreviate) {
-        chunks.push({ text: separator, style: "gray" });
+      if (opt.abbreviate) {
+        chunks.push({ text: ",", style: "gray" });
       } else {
         // If this is the second-to-last element, add an "or"
         if (index + 2 === superposition.length) {
-          chunks.push({ text: `${separator}or `, style: "gray" });
+          chunks.push({ text: `, or `, style: "gray" });
         } else {
-          chunks.push({ text: separator, style: "gray" });
+          chunks.push({ text: ", ", style: "gray" });
         }
       }
     }
   });
 
   // Next line
-  chunks.push({ text: OPTIONS.abbreviate ? "\n" : "\n\n" });
+  chunks.push({ text: opt.abbreviate ? "\n" : "\n\n" });
 
   // Players
   state.players.forEach((player) => {
     chunks.push(
-      ...getPlayerChunks(player, player.order === state.activePlayer),
+      ...getPlayerChunks(player, opt, player.order === state.activePlayer),
     );
     chunks.push({ text: "\n" });
   });
@@ -164,7 +164,13 @@ function getStateChunks(state: CurrentGameState | PastGameState): slChunk[] {
 //   // prettier-ignore
 //   const state: CurrentGameState = { "turn": 11, "activePlayer": 0, "activeQuark": ["up", "top", "charm"], "players": [{ "order": 0, "name": "alice", "chamber": ["charm"], "score": 8 }, { "order": 1, "name": "bob", "chamber": ["charm", "up", "up", "bottom", "strange"], "score": 4 }], "timeline": [{ "turn": 1, "activePlayer": 0, "activeQuark": ["down", "up", "bottom"], "players": [{ "order": 0, "name": "alice", "chamber": ["down", "strange", "charm", "up"], "score": 0 }, { "order": 1, "name": "bob", "chamber": ["charm", "down", "up", "up"], "score": 0 }], "observation": { "activeFlavor": "down", "reaction": "hadronized", "observer": 0 } }, { "turn": 2, "activePlayer": 1, "activeQuark": ["strange", "bottom", "down"], "players": [{ "order": 0, "name": "alice", "chamber": ["strange", "charm", "up"], "score": 2 }, { "order": 1, "name": "bob", "chamber": ["charm", "down", "up", "up"], "score": 0 }], "observation": { "activeFlavor": "down", "reaction": "hadronized", "observer": 1 } }, { "turn": 3, "activePlayer": 0, "activeQuark": ["strange", "up", "charm"], "players": [{ "order": 0, "name": "alice", "chamber": ["strange", "charm", "up"], "score": 2 }, { "order": 1, "name": "bob", "chamber": ["charm", "up", "up"], "score": 2 }], "observation": { "activeFlavor": "charm", "reaction": "hadronized", "observer": 0 } }, { "turn": 4, "activePlayer": 1, "activeQuark": ["down", "top", "strange"], "players": [{ "order": 0, "name": "alice", "chamber": ["strange", "up"], "score": 4 }, { "order": 1, "name": "bob", "chamber": ["charm", "up", "up"], "score": 2 }], "observation": { "activeFlavor": "strange", "reaction": "no reaction", "observer": 1 } }, { "turn": 5, "activePlayer": 0, "activeQuark": ["charm", "up", "strange"], "players": [{ "order": 0, "name": "alice", "chamber": ["strange", "up"], "score": 4 }, { "order": 1, "name": "bob", "chamber": ["charm", "up", "up", "strange"], "score": 2 }], "observation": { "activeFlavor": "strange", "reaction": "hadronized", "observer": 0 } }, { "turn": 6, "activePlayer": 1, "activeQuark": ["strange", "down", "top"], "players": [{ "order": 0, "name": "alice", "chamber": ["up"], "score": 6 }, { "order": 1, "name": "bob", "chamber": ["charm", "up", "up", "strange"], "score": 2 }], "observation": { "activeFlavor": "strange", "reaction": "hadronized", "observer": 1 } }, { "turn": 7, "activePlayer": 0, "activeQuark": ["up", "bottom", "top"], "players": [{ "order": 0, "name": "alice", "chamber": ["up"], "score": 6 }, { "order": 1, "name": "bob", "chamber": ["charm", "up", "up"], "score": 4 }], "observation": { "activeFlavor": "up", "reaction": "hadronized", "observer": 0 } }, { "turn": 8, "activePlayer": 1, "activeQuark": ["down", "top", "bottom"], "players": [{ "order": 0, "name": "alice", "chamber": [], "score": 8 }, { "order": 1, "name": "bob", "chamber": ["charm", "up", "up"], "score": 4 }], "observation": { "activeFlavor": "bottom", "reaction": "no reaction", "observer": 1 } }, { "turn": 9, "activePlayer": 0, "activeQuark": ["charm", "top", "bottom"], "players": [{ "order": 0, "name": "alice", "chamber": [], "score": 8 }, { "order": 1, "name": "bob", "chamber": ["charm", "up", "up", "bottom"], "score": 4 }], "observation": { "activeFlavor": "charm", "reaction": "no reaction", "observer": 0 } }, { "turn": 10, "activePlayer": 1, "activeQuark": ["strange", "bottom", "charm"], "players": [{ "order": 0, "name": "alice", "chamber": ["charm"], "score": 8 }, { "order": 1, "name": "bob", "chamber": ["charm", "up", "up", "bottom"], "score": 4 }], "observation": { "activeFlavor": "strange", "reaction": "no reaction", "observer": 1 } }] };
 
-//   sl(getStateChunks(state));
+//   const options: Options = {
+//     abbreviate: false,
+//     showEmpty: false,
+//     showPlayerOrder: true,
+//   };
+
+//   sl(getStateChunks(state, options));
 // }
 
 // main();
