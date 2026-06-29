7cef9238f6044b339a83f2875f4fb382b2f99ae3
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-06-29 15:21:26 -0400

    very WIP chambers in UI

--- src/lib/components/Quark.svelte
+++ src/lib/components/Quark.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import type { Flavor, Quark } from "../Quark.ts";
+    import type { Flavor, Quark, QuarkStatus } from "../Quark.ts";
     import { Spring } from "svelte/motion";
 
     interface Props {
         quark: Quark;
+        status: QuarkStatus;
         x: number;
         y: number;
         onmousedown?: () => void;
     }
 
-    let { quark, x, y, onmousedown }: Props = $props();
+    let { quark, status, x, y, onmousedown }: Props = $props();
 
     const COLOR_MAP: Record<Flavor, string> = {
         up: "#5dafef", // blue
@@ -36,16 +37,19 @@
     });
 
     onMount(() => {
-        pos.set({ x: window.innerWidth / 2, y: window.innerHeight / 2 });
+        pos.set({
+            x: window.innerWidth / 2 - 25,
+            y: window.innerHeight / 2 - 25,
+        });
     });
 </script>
 
 <span
     id="quark-{quark.index}"
     class="quark"
-    data-status={quark.status}
-    style:left="{pos.current.x - 25}px"
-    style:top="{pos.current.y - 25}px"
+    data-status={status}
+    style:left="{pos.current.x}px"
+    style:top="{pos.current.y}px"
     {onmousedown}
     role="button"
     tabindex="0"
@@ -67,6 +71,8 @@
 
         width: 50px;
         height: 50px;
+
+        user-select: none;
 
         &[data-status="latent"] {
             display: none;
@@ -114,7 +120,6 @@
             font-size: 2rem;
             text-align: center;
             text-shadow: 1px 1px slategray;
-            user-select: none;
         }
     }
 </style>
--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,18 +1,54 @@
 <script lang="ts">
     import Reset from "../../lib/components/Reset.svelte";
     import Quark from "../../lib/components/Quark.svelte";
+    import Chamber, {
+        type Section,
+        type Rect,
+    } from "../../lib/components/Chamber.svelte";
 
     import { Hadronize } from "../../lib/Hadronize.ts";
     import { prngDriver } from "../../lib/drivers/prng.ts";
+    import { onMount } from "svelte";
+    import type { QuarkStatus } from "../../lib/Quark.ts";
 
-    const game = new Hadronize(1, [
+    const game = new Hadronize(2, [
         { name: "p1", driver: prngDriver },
         { name: "p2", driver: prngDriver },
+        { name: "p3", driver: prngDriver },
+        { name: "p4", driver: prngDriver },
     ]);
 
-    let quarks = $state(
+    interface QuarkDatum {
+        x: number;
+        y: number;
+        index: number;
+        status: QuarkStatus;
+        owner: number | undefined;
+    }
+
+    let quarks: QuarkDatum[] = $state(
         game.quarks.map((q) => {
-            return { quark: q, x: 0, y: 0 };
+            let owner = undefined;
+            game.players.forEach((player) => {
+                player.chamber.indices.forEach((index) => {
+                    if (index === q.index) owner = player.order;
+                });
+            });
+            return { index: q.index, status: q.status, x: 0, y: 0, owner };
+        }),
+    );
+
+    interface ChamberDatum {
+        order: number;
+        sections: Partial<Record<Section, Rect>>;
+    }
+
+    let chambers: ChamberDatum[] = $state(
+        game.players.map((p) => {
+            return {
+                order: p.order,
+                sections: {},
+            };
         }),
     );
 
@@ -27,10 +63,83 @@
         mouse = { x: event.clientX, y: event.clientY };
 
         if (superposedQuarkPressed) {
-            superposed.x = mouse.x;
-            superposed.y = mouse.y;
+            superposed.x = mouse.x - 25;
+            superposed.y = mouse.y - 25;
+
+            chambers.forEach((chamber) => {
+                if (!chamber.sections.top || !chamber.sections.hadron) return;
+
+                const leftEdge = chamber.sections.top.left;
+                const rightEdge =
+                    chamber.sections.hadron.left +
+                    chamber.sections.hadron.width;
+                const topEdge = chamber.sections.top.top;
+                const bottomEdge =
+                    chamber.sections.top.top + chamber.sections.top.height;
+
+                if (
+                    superposed.x > leftEdge &&
+                    superposed.x < rightEdge &&
+                    superposed.y > topEdge &&
+                    superposed.y < bottomEdge
+                ) {
+                    const quark = game.quarks[superposed.index];
+                    quark.collapse();
+                    superposed.owner = chamber.order;
+
+                    game.players[chamber.order].chamber.indices.push(
+                        quark.index,
+                    );
+                    superposedQuarkPressed = false;
+                    game.superposedIndex = undefined;
+
+                    game.produceQuark();
+                    game.superposedIndex = game.superposedIndex;
+                    superposed = quarks[game.superposedIndex!];
+
+                    update();
+                }
+            });
         }
     }
+
+    function update() {
+        chambers.forEach((c) => {
+            if (c.sections) {
+                const indicies = game.players[c.order].chamber.indices;
+                const hadrons = game.players[c.order].chamber.hadrons;
+                hadrons.forEach((h) => {
+                    h.indices.forEach((index) => {
+                        const q = quarks[index];
+                        const rect = c.sections["hadron"];
+                        if (rect) {
+                            q.x =
+                                Math.random() * (rect.width - 50) +
+                                rect.left -
+                                25;
+                            q.y =
+                                Math.random() * (rect.height - 50) +
+                                rect.top -
+                                25;
+                        }
+                    });
+                });
+                indicies.forEach((index) => {
+                    const q = quarks[index];
+                    const rect = c.sections[game.quarks[q.index].flavor];
+                    if (rect) {
+                        q.x = rect.left + rect.width / 2 - 25;
+                        q.y = rect.top;
+                    }
+                });
+            }
+        });
+        quarks.forEach((q) => {
+            q.status = game.quarks[q.index].status;
+        });
+    }
+
+    onMount(update);
 </script>
 
 <svelte:head>
@@ -44,10 +153,13 @@
 
 <Reset />
 
+{JSON.stringify(game.players[0].chamber)}
+
 <div id="quarks">
     {#each quarks as q, index}
         <Quark
-            quark={q.quark}
+            quark={game.quarks[q.index]}
+            status={q.status}
             x={q.x}
             y={q.y}
             onmousedown={() => {
@@ -58,8 +170,23 @@
     {/each}
 </div>
 
+<div id="chambers">
+    {#each chambers as chamber}
+        <Chamber
+            owner={game.players[chamber.order]}
+            setSections={(sections: Partial<Record<Section, Rect>>) =>
+                (chamber.sections = sections)}
+        />
+    {/each}
+</div>
+
 <h1>Play Hadronize</h1>
 <p>This page is for playing Hadronize</p>
 
 <style lang="scss">
+    #chambers {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
 </style>
