f3699494a724756b0607bb0e2c95577e4f7f0958
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-07-11 13:52:15 -0400

    add mdsvex

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import adapter from "@sveltejs/adapter-static";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { mdsvex } from "mdsvex";
 
 export default defineConfig({
   plugins: [
     sveltekit({
+      extensions: [".svelte", ".md"],
+      preprocess: [
+        mdsvex({
+          extensions: [".md"],
+        }),
+      ],
       compilerOptions: {
         // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
         runes: ({ filename }) =>
