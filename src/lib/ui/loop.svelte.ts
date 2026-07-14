483bb8c0a5e3180b1b557d542f7fe69c65c130ea
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-07-14 01:48:17 -0400

    fix winning animation

--- src/lib/ui/loop.svelte.ts
+++ src/lib/ui/loop.svelte.ts
@@ -103,22 +103,26 @@ export class LoopManager {
 
     this.mouse.dropIndicator.active = false;
 
+    if (typeof this.store.result === "number") {
+      const winningChamber = this.store.chambers[this.store.result];
+
+      winningChamber.hovered = false;
+      winningChamber.layoutMode = "full";
+      this.layout.update();
+
+      winningChamber.x = this.layout.container.x;
+      winningChamber.y = this.layout.container.y;
+      this.layout.placeQuarks(winningChamber);
+
+      winningChamber.label.color = "#98c379";
+      winningChamber.label.text += " Wins!";
+      this.layout.placeChamberLabel(winningChamber);
+    }
+
     const chambersToExplode = this.store.chambers.filter(
       (c) => c.order !== this.store.result,
     );
     chambersToExplode.forEach((c) => this.layout.explodeChamber(c));
-
-    if (typeof this.store.result === "number") {
-      const winningChamber = this.store.chambers[this.store.result];
-      winningChamber.x = this.layout.container.x;
-      winningChamber.y = this.layout.container.y;
-      winningChamber.hovered = false;
-      winningChamber.layoutMode = "full";
-      this.layout.placeQuarks(winningChamber);
-      this.layout.placeChamberLabel(winningChamber);
-      winningChamber.label.color = "#98c379";
-      winningChamber.label.text += " Wins!";
-    }
   }
 
   public async start(): Promise<void> {
