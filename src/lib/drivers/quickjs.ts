2a10b977d5e31f72d827bedbbad7102fe6d2f015
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-07-15 20:01:46 -0400

    replace math.random with mulberry32 for quickjs drivers

--- src/lib/drivers/quickjs.ts
+++ src/lib/drivers/quickjs.ts
@@ -10,6 +10,18 @@ class QuickJSError {
 }
 
 /**
+ * Simple hash for converting strings into 32-bit integers (the format that
+ * mulberry32 takes as its seed)
+ */
+function djb2Hash(str: string): number {
+  let hash = 5381;
+  for (let i = 0; i < str.length; i++) {
+    hash = (hash << 5) + hash + str.charCodeAt(i);
+  }
+  return hash >>> 0; // Force hash num into a 32-bit unsigned integer
+}
+
+/**
  * Adapted from
  *
  * https://github.com/ethmarks/nolet/blob/main/src/lib/runSnippet.ts
@@ -22,10 +34,12 @@ function runSnippet(
   const vm = QuickJS.newContext();
 
   //
-  // Adding state object
+  // Add state object
   //
 
-  const jsonHandle = vm.newString(JSON.stringify(state));
+  const stateJSON = JSON.stringify(state);
+
+  const jsonHandle = vm.newString(stateJSON);
 
   const stateHandle = vm
     .unwrapResult(vm.evalCode(`JSON.parse`))
@@ -40,8 +54,30 @@ function runSnippet(
   vm.setProp(vm.global, "state", stateHandle);
   stateHandle.dispose();
 
+  //
+  // Replace Math.random with custom PRNG
+  //
+
+  let seed = djb2Hash(stateJSON);
+
+  const mulberryHandle = vm.newFunction("prng", () => {
+    // https://github.com/cprosche/mulberry32
+    let t = (seed += 0x6d2b79f5);
+    t = Math.imul(t ^ (t >>> 15), t | 1);
+    t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+    const val = ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+
+    return vm.newNumber(val);
+  });
+
+  const mathHandle = vm.getProp(vm.global, "Math");
+
+  vm.setProp(mathHandle, "random", mulberryHandle);
+
+  mathHandle.dispose();
+  mulberryHandle.dispose();
+
   // Remove non-deterministic functions from the global object.
-  vm.evalCode("delete Math.random;");
   vm.evalCode("delete Date;");
   vm.evalCode("delete performance;");
 
@@ -100,11 +136,6 @@ function runSnippet(
       ) {
         // The user might have tried to use one of the built-in functions that
         // we removed.
-        if (code.includes("Math.random")) {
-          return new QuickJSError(
-            "It looks like you tried to use `Math.random()`. `Math.random()` is unavailable because it produces non-deterministic outputs, which are disallowed in pure functional programming. Find another way to approach the problem.",
-          );
-        }
         if (code.includes("Date")) {
           return new QuickJSError(
             "It looks like you tried to use `Date`. `Date` is unavailable because it can produce non-deterministic outputs, which are disallowed in pure functional programming. Find another way to approach the problem.",
